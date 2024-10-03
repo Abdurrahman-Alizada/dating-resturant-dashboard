861349591938dbe1e9bf64bf258eeb9e2fbee49b
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-10-03 14:09:18 +0500

    aksld

--- src/Component/Pages/ListYourResturant/ListYourResturantIndex.tsx
+++ src/Component/Pages/ListYourResturant/ListYourResturantIndex.tsx
@@ -197,7 +197,7 @@ function ListYourResturantIndex() {
         address: address,
         latitude: latitude,
         numberOfPeople: numberOfPeople,
-        startingPrice: price,
+        price: price,
         atmosphere: selectedAtmosphere,
         cuisines: selectedCuisine,
         longitude: longitude,
@@ -408,7 +408,7 @@ function ListYourResturantIndex() {
               Starting from price
             </label>
             <input
-              type="number"
+              type="text"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               value={price}
               onChange={(e) => setPrice(Number(e.target.value))}
